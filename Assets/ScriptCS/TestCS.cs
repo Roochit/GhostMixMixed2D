64f2a65f3260df4ae6918418d7d5e36cbe5ff1e7
Roochit Khamcharoen <roochit.khamcharoen@gmail.com>
2026-05-11 20:30:33 +0700

    Test CS

---
+++ Assets/ScriptCS/TestCS.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCS : MonoBehaviour
+{
+    int Testpoint = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Testpoint <= 10)
+        {
+            Debug.Log("teat is : " + Testpoint);
+            Testpoint++;
+        }
+
+    }
+}
