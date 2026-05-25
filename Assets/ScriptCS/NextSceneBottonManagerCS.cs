5f5ff03e184bb0b5b95d65a09860f9f63c26adb0
Roochit Khamcharoen <roochit.khamcharoen@gmail.com>
2026-05-25 19:40:33 +0700

    เสียงตอนเข้าเกม และ ตอนชนะ V.Bata

--- Assets/ScriptCS/NextSceneBottonManagerCS.cs
+++ Assets/ScriptCS/NextSceneBottonManagerCS.cs
@@ -7,12 +7,14 @@ using UnityEngine.SceneManagement;
 public class NextSceneBottonManagerCS : MonoBehaviour
 {
     public string NextScene ;
-    // public string sceneName; // พิมพ์ชื่อ Scene ที่จะไปใน Inspector
-    public float delayTime = 0.2f; // ระยะเวลาขยาย-ย่อ
-    // Start is called before the first frame update
+    
+    public float delayTime = 0.2f; 
+    private AudioSource NextSceneAudio;
+    
     void Start()
     {
         Time.timeScale = 1f; 
+        NextSceneAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,24 +30,33 @@ public class NextSceneBottonManagerCS : MonoBehaviour
 
     IEnumerator PlayAnimationAndLoad()
     {
-        // 1. เล่นอนิเมชั่นขยาย (ใช้โค้ดคุม Scale)
+
+        PlayNextSceneSound();
+        
         transform.localScale = new Vector3(6.0f, 6.0f, 6.0f);
         
-        // รอแป๊บหนึ่ง
+        
         yield return new WaitForSeconds(delayTime / 2);
         
-        // 2. เล่นอนิเมชั่นย่อกลับ
+        
         transform.localScale = new Vector3(4.0f, 4.0f, 4.0f);
         
         yield return new WaitForSeconds(delayTime / 2);
 
-        // 3. เปลี่ยนหน้า
-        // SceneManager.LoadScene(sceneName);
+   
         SceneManager.LoadScene(NextScene);
     }
 
-    // void NextSceneFunction()
-    // {
-    //     SceneManager.LoadScene(NextScene);
-    // }
+    public void PlayNextSceneSound()
+    {
+        if (NextSceneAudio != null)
+        {
+            NextSceneAudio.PlayOneShot(NextSceneAudio.clip);
+        }
+        else
+        {
+            Debug.LogWarning("ลืมใส่ Component AudioSource หรือเปล่าเพื่อน !");
+            Debug.LogWarning("หรือตรงนี้ๆไม่ต้องหารใช้ Component AudioSource ?");
+        }
+    }
 }
